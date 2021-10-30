f470fd58de5dcccb06c994c1692b138e3a08be55
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-10-30 23:35:23 +0530

    Create B - Xor special lst - T.cpp

---
+++ Day01/B - Xor special lst - T.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Round 752 Div 2
+Approach :- The basic idea is for even size array we can choose subarray of size '1' and LIS is equal to 1 and xor of 1 even number of times is 0 ,
+            for odd we need to find a subarray of size 2 whose LIS is 1 then it is possible else it is not.
+*/
