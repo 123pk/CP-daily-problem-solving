9361f50afa44ac199ae9d76507504fd0ba32917c
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-10-31 21:51:34 +0530

    Create Help Nishant.cpp

---
+++ Day02/Help Nishant.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Codechef
+Contest :- Codechef Snackdown Round 1B
+Approach :- If you will carefully analyse you will find that for a given n we need the nearest (x*(x+1))/2 value and answer is 2*x
+*/
