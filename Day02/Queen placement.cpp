71720c7a9651394ab216d5e74cf50fb57d4817bf
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-10-31 21:49:34 +0530

    Update Queen placement.cpp

--- Day02/Queen placement.cpp
+++ Day02/Queen placement.cpp
@@ -3,3 +3,25 @@ Platform :- Codechef
 Contest :- Codechef Snackdown Round 1B
 Approach :- Basic approach is to put N-2 queen on mail diagonal , first queen on top left(1,1) and then from (3,3) to (n,n) put the remaining ones
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+         int n;
+         cin>>n;
+         
+         vector<vector<char>>A(n,vector<char>(n,'.'));
+         A[1][1]='Q';
+         for(int i=3;i<n;i++){
+             A[i][i]='Q';
+         }
+         for(auto &x:A){
+             for(auto &y:x)cout<<y<<"";
+             cout<<"\n";
+         }
+    }
+}
