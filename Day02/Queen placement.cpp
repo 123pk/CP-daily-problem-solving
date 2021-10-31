e5cb919bc9a315d731b29be14a8cb97ad1adca0f
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-10-31 21:49:14 +0530

    Create Queen placement.cpp

---
+++ Day02/Queen placement.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Codechef
+Contest :- Codechef Snackdown Round 1B
+Approach :- Basic approach is to put N-2 queen on mail diagonal , first queen on top left(1,1) and then from (3,3) to (n,n) put the remaining ones
+*/
