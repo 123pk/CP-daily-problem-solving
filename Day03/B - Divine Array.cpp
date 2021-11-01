ff33c8a9daa3def253fbc8950bd400995318dfe6
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-11-01 20:04:14 +0530

    Create B - Divine Array.cpp

---
+++ Day03/B - Divine Array.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Round 752 Div 2
+Approach :- After some itteration the array becomes non changing , so for each itteration we store the array in map and for some k >=d where 'd' is the max number of itteration
+            then we took that last array and for all other values we print the 'xth' element.
+*/
