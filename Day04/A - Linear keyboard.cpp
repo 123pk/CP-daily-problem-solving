b1502b85a35ca26bb6426d808bea8f761f88fdad
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-11-02 23:03:01 +0530

    Update A - Linear keyboard.cpp

--- Day04/A - Linear keyboard.cpp
+++ Day04/A - Linear keyboard.cpp
@@ -3,3 +3,28 @@ Platform :- Codeforces
 Contest :- Codeforces Round 753 Div 3
 Approach :- Store the value of each charter in a vector and now for given string find the adjacent different between values and add to our answer
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        string s,p;
+        cin>>s>>p;
+        
+        vector<int>z(26);
+        for(int i=0;i<26;++i){
+            z[s[i]-'a']=i+1;
+        }
+        
+        int last=z[p[0]-'a'];
+        int ans=0;
+        for(int i=1;i<p.size();++i){
+            ans+=abs(last-z[p[i]-'a']);
+            last=z[p[i]-'a'];
+        }
+        cout<<ans<<"\n";
+    }
+}
