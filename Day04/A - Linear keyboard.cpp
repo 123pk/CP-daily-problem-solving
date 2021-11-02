cbf109dce8539c75061c6b1db75659498981fb8d
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-11-02 23:02:26 +0530

    Update A - Linear keyboard.cpp

--- Day04/A - Linear keyboard.cpp
+++ Day04/A - Linear keyboard.cpp
@@ -1,1 +1,5 @@
-
+/*
+Platform :- Codeforces
+Contest :- Codeforces Round 753 Div 3
+Approach :- Store the value of each charter in a vector and now for given string find the adjacent different between values and add to our answer
+*/
