1b841a0070b3d01fb93fc382f9d48d0bab6e4425
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-11-02 23:13:45 +0530

    Create B -  Odd grasshopper.cpp

---
+++ Day04/B -  Odd grasshopper.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Round 753 Div 3
+Approach :- If you will analyse carefully you will find that after every 4 itteration you came back to same place where you started from so all problem comes down to itterating
+            (n % 4) times from ith ..to nth values .
+*/
