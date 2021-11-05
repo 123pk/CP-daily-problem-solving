d111a29871d7d9394f2dec612ae07021b3602c7f
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-11-05 23:18:56 +0530

    Create Global warming check.cpp

---
+++ Day07/Global warming check.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Topcoder
+Contest :- SRM 818
+Approach :- First create the array now using sliding window find all the valid subarray of size k and store in vector of pair with values and starting index now find the smallest value
+            with smallest index and largest value with largest index and return ans with those indexes.
+*/
