b2bfe5d74cb16307036cd0ddab4e934c8fc6d165
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-11-06 21:43:15 +0530

    Update Single number III.cpp

--- Day08/Single number III.cpp
+++ Day08/Single number III.cpp
@@ -1,3 +1,45 @@
 /*
 Platform :- Leetcode
 */
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        
+        vector<int>Q;
+        int c=1;
+        for(int i=0;i<nums.size();++i)
+        { 
+            if(i==(nums.size()-1))
+            {  
+                if(c==1)
+                {
+                    Q.push_back(nums[i]);
+                }
+                
+            }
+         else
+         {
+             if(nums[i]==nums[i+1])
+             {
+                 c++;
+             }
+             else
+             {
+                if(c==1)
+                {
+                 Q.push_back(nums[i]);
+                 } 
+                 else
+                 {
+                     c=1;
+                 }
+             }
+             
+             
+         }
+           
+        }
+        return Q;
+    }
+};
