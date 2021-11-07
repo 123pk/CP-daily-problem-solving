94c89516de42095a04fcd7c82e7d1afc890e7836
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-11-07 21:10:42 +0530

    Create C - Martial Artist.cpp

---
+++ Day09/C - Martial Artist.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Atcoder
+Contest :- Atcoder Contest 226
+Approach :- We do bfs from all the values required for nth skill and do maintain an used where where we store the skill required and add that time taken at the end to our answer
+*/
