73c731e92313faf36df8999a75fd31a556b5c4d8
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-11-07 21:01:00 +0530

    Create A - Round Decimals.cpp

---
+++ Day09/A - Round Decimals.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Atcoder
+Contest :- Atcoder Beginner Contest 226
+Approach:- check the first charcter after '.' and if it is >=5 then increment value before '.' by 1 else print it as it is.
+*/
