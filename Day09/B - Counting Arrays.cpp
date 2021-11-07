f9e78ab31a7a03a6146d5c4bab271da8058a16e7
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-11-07 21:05:12 +0530

    Update B - Counting Arrays.cpp

--- Day09/B - Counting Arrays.cpp
+++ Day09/B - Counting Arrays.cpp
@@ -3,3 +3,27 @@ Platform :- Atcoder
 Contest :- Atcoder Beginner Contest 226
 Approach :- Simply use set of vector and print the size of the set
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+     int n;
+     cin>>n;
+     
+     set<string>unq;
+     string s;
+     int d=0;
+     long long x;
+     for(int i=0;i<n;++i){
+         int l;
+         cin>>l;
+         s="";
+         for(int j=0;j<l;++j){
+             cin>>x;
+             s+=to_string(x);
+             s+=',';
+         }
+          unq.insert(s);
+     }
+     cout<<(unq.size()+d)<<"\n";
+}
