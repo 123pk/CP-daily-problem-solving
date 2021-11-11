e7a14dea800e9a7079a58bf19fae8b4bc83691e1
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-11-11 21:23:17 +0530

    Update Best time to buy and sell stock II .cpp

--- Day12/Best time to buy and sell stock II .cpp
+++ Day12/Best time to buy and sell stock II .cpp
@@ -3,3 +3,26 @@ Platform :- Leetcode
 Approach :- As we can sell and buy stock on same day so we will do store the stock value in stack and if the value of stack is greater than the current stock price then we remove the
             top value of stack and add the current stock price to the stack else we will sell the stock and add the profit to our answer , and replace stack top with current stock value. 
 */
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int ans=0;
+        stack<int>P;
+        for(int i=0;i<prices.size();++i){
+            if(i==0)P.push(prices[i]);
+            else {
+                if(P.top()>=prices[i]){
+                    P.pop();
+                    P.push(prices[i]);
+                }
+                else{
+                    ans+=(prices[i]-P.top());
+                    P.pop();
+                    P.push(prices[i]);
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
