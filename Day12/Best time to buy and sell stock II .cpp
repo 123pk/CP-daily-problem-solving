4d8959147c3368bbc6d5a75605ef7f031284461c
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-11-11 21:22:34 +0530

    Create Best time to buy and sell stock II .cpp

---
+++ Day12/Best time to buy and sell stock II .cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Leetcode
+Approach :- As we can sell and buy stock on same day so we will do store the stock value in stack and if the value of stack is greater than the current stock price then we remove the
+            top value of stack and add the current stock price to the stack else we will sell the stock and add the profit to our answer , and replace stack top with current stock value. 
+*/
