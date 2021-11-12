aeded18d6b1cd948ce0a217a4117d006d9b403b9
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-11-12 22:44:38 +0530

    Update A - A.m. deviation.cpp

--- Day13/A - A.m. deviation.cpp
+++ Day13/A - A.m. deviation.cpp
@@ -1,5 +1,5 @@
 /*
 Platform :- Codeforces
 Contest:-  Codeforces Round 754 
-Approach :- 
+Approach :- It is optimal to choose either a or c and decrease or increase them and choose ''b' and accordingly increase or decrease . The value of ( a*c - 2*b ) differ by 3 .
 */
