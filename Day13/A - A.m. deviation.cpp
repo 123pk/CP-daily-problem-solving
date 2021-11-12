af3712ded9f2b8775f8cbb8ae0f4c56433467e06
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-11-12 22:44:49 +0530

    Update A - A.m. deviation.cpp

--- Day13/A - A.m. deviation.cpp
+++ Day13/A - A.m. deviation.cpp
@@ -3,3 +3,37 @@ Platform :- Codeforces
 Contest:-  Codeforces Round 754 
 Approach :- It is optimal to choose either a or c and decrease or increase them and choose ''b' and accordingly increase or decrease . The value of ( a*c - 2*b ) differ by 3 .
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        long long a,b,c;
+        cin>>a>>b>>c;
+        
+        long long dif=(a+c)-(2*b);
+        if(dif==0)cout<<0<<"\n";
+        else{
+            long long ans;
+            //if dif > 0 or dif<0
+            long long z=abs(dif/3);
+            if(dif<0){
+                dif+=(z*3);
+                ans=abs(dif);
+                dif+=3;
+                ans=min(ans,abs(dif));
+            }
+            else{
+                dif-=abs(z*3);
+                ans=dif;
+                dif-=3;
+                ans=min(ans,abs(dif));
+            }
+            cout<<ans<<"\n";
+        }
+        
+    }
+}
