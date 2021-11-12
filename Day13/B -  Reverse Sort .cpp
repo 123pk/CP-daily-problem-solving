6653a8bb73c7fe8afc6fee382f9ab2e0a7d86274
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-11-12 22:52:10 +0530

    Create B -  Reverse Sort .cpp

---
+++ Day13/B -  Reverse Sort .cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Round 754
+Approach :- If you will analyse the problem you will find that the value if the sorted string is not equal to given string then we can make them equal in just one move . So we will
+            add all the index where the s[i] is not equal to sorted string value.
+*/
