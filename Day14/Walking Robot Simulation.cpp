31beabac319054e4bfcda70eaea42843cf826b26
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-11-13 21:56:45 +0530

    Create Walking Robot Simulation.cpp

---
+++ Day14/Walking Robot Simulation.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Leetcode
+Contest :- Leetcode Weekly contest 65
+Approach :- We can always move num % (h+w-2)*2 values , because after (h+w-2)*2 operation we came back to same place where we started , take care when we start from 0,0 and came 
+            back to 0,0 here the direction changes from east to south
+*/
