33498eeac5bb3eaff6b792abc175e91e311007a3
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-11-13 21:54:20 +0530

    Create Check whether two strings are equivalent.cpp

---
+++ Day14/Check whether two strings are equivalent.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Leetcode
+Contest :- Leetcode bi weekly contest 65
+Approach :- maintain frequency array for all alphabets of two string and check if the condition is broken or not.
+*/
