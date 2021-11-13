13fd09c43ba5919ce39d0410a1422cb2d0df8c7e
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-11-13 21:54:33 +0530

    Update Check whether two strings are equivalent.cpp

--- Day14/Check whether two strings are equivalent.cpp
+++ Day14/Check whether two strings are equivalent.cpp
@@ -3,3 +3,17 @@ Platform :- Leetcode
 Contest :- Leetcode bi weekly contest 65
 Approach :- maintain frequency array for all alphabets of two string and check if the condition is broken or not.
 */
+class Solution {
+public:
+    bool checkAlmostEquivalent(string word1, string word2) {
+        vector<int>A(26),B(26);
+        for(auto &x:word1)A[x-'a']++;
+        
+        for(auto &x:word2)B[x-'a']++;
+        
+        for(int i=0;i<26;++i){
+            if(abs(A[i]-B[i])>3)return false;
+        }
+        return true;
+    }
+};
