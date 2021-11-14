a8238d6175a0f4a1e27c3ac54bf1cc9688a957da
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-11-14 17:23:55 +0530

    Create Painter.cpp

---
+++ Day16/Painter.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Coding Competition with Google
+Approach :- First creat a vector of character which store the color required for creating all the combinations like for 'A'(Grey) it will store {'R','B','Y'} and so on 
+            now we will look for how many times we need to color the boxes with each color individually , like for sequence GPR , we will color all 3 three cell in 1 time , 
+            cell 1 and 2 with blue color in 1 time and cell 1 with color yellow in 1 time hence answer is 3 for this.
+*/
