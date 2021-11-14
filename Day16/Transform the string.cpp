885a7fb82e1e78e0bc7e078a665c6b2b109ceb4c
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-11-14 16:59:14 +0530

    Create Transform the string.cpp

---
+++ Day16/Transform the string.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Coding Competitions with Google
+Approach :- For each character in string 's' , we will find the number of operations we will need to covert to some character of 'f' for all characters of 'f' since string f can have
+            atmax 26 characters hence this will take O(N*26) in worst time which is well inside our limit.
+*/
