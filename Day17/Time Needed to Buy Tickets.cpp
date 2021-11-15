6cd354f833ea87e62baaeb90653ed3390d6bd50e
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-11-15 22:52:19 +0530

    Update Time Needed to Buy Tickets.cpp

--- Day17/Time Needed to Buy Tickets.cpp
+++ Day17/Time Needed to Buy Tickets.cpp
@@ -3,3 +3,36 @@ Platform :- Leetcode
 Contest :- Leetcode Weekly contest 267
 Approach :- repeatedly perform the operations as said in problem statement
 */
+class Solution {
+public:
+    int timeRequiredToBuy(vector<int>& tickets, int k) {
+        set<int>temp;
+        for(int i=0;i<tickets.size();++i){
+            temp.insert(i);
+        }
+        
+        int ans=0,f=0;
+        vector<int>z;
+        while(!temp.empty()){
+            z.clear();
+            for(auto &x:temp){
+                tickets[x]--;
+                ans++;
+                if(tickets[x]==0){
+                    if(x==k){
+                        f=1;
+                        break;
+                    }
+                    z.push_back(x);
+                }
+            }
+            if(f)break;
+            
+            //I WANT TO REMOVE THE VALUES WHICH ARE ZERor
+            for(int i=z.size()-1;i>=0;--i)temp.erase(z[i]);
+            
+            
+        }return ans;
+         
+    }
+};
