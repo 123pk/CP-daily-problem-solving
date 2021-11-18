909305e0ac8d7947f0a8845791465ef3ed9c0a44
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-11-18 23:36:10 +0530

    Create GCD of prefixes.cpp

---
+++ Day20/GCD of prefixes.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Codechef
+Contest :- Codechef Starters 17
+Approach :- The first element of GCD Array is going to be the same for oringinal array , now we will find gcd of prefix and compare with the current element A[i] , if for any index
+            GCD_of_prefix[i]<A[i] then we cannot have any such array else we print the same given array.
+*/
