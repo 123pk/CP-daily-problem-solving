61425a1e763a847a14f515bad6623cb5121f3653
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-11-18 23:08:25 +0530

    Create New Peice.cpp

---
+++ Day20/New Peice.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Codechef
+Contest :- Codechef starters 17
+Approach :- IF both points are same then 0 , else if the sum%2 of both points are same then we can move in 2 operations else we need 1 operations.
+*/
