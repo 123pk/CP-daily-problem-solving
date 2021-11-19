f2f394ace830d1acdc5f786ff159239dd642b77e
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-11-19 21:14:48 +0530

    Create Hamming Distance.cpp

---
+++ Day21/Hamming Distance.cpp
@@ -0,0 +1,4 @@
+/*
+Platform :- Leetcode
+Appraoch :- first convert the given number into 32 bit string representation and check for all the 32 bits that are not equal and count of those is our answer
+*/
