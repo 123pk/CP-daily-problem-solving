b9e66850f27fcb3bf0058c4dd8dd1f71567c501a
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-11-19 21:15:06 +0530

    Update Hamming Distance.cpp

--- Day21/Hamming Distance.cpp
+++ Day21/Hamming Distance.cpp
@@ -2,3 +2,16 @@
 Platform :- Leetcode
 Appraoch :- first convert the given number into 32 bit string representation and check for all the 32 bits that are not equal and count of those is our answer
 */
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        string temp=bitset<32>(x).to_string();
+        string temp2 = bitset<32>(y).to_string();
+        
+        int ans=0;
+        for(int i=0;i<32;++i){
+            if(temp[i]!=temp2[i])ans++;
+        }
+        return ans;
+    }
+};
