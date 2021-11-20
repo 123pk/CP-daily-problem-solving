8ac5dd84b6dc1ee20f684e53846f39cbf7f16320
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-11-20 22:42:58 +0530

    Create D - The Number of imposters.cpp

---
+++ Day22/D - The Number of imposters.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Round 747 Div 2
+Approach :- Try to color the graph with two colors and if there is any contradictions that occurs which doesnot satisfy the arrangment of nodes then print -1 else print 
+             the max no of colors of any type in all the disjoint set or explicit graphs.
+*/
