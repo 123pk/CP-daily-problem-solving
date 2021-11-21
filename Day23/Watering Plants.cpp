6d15834d12f70b6d326b02f0315bfae8fc0ea65b
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-11-21 19:57:51 +0530

    Create Watering Plants.cpp

---
+++ Day23/Watering Plants.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Leetcode
+Contest :- Leetcode Weekly contest 268
+Approach :- We will use bruteforce approach where we will fill our can with full capacity and water the plants if the amoount of water in can is less than the water plant require
+            then we will go back to river and continue the process.
+*/
