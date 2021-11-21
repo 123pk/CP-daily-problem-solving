5c01896ab3be030fff1782e006cfa86ce971e44c
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-11-21 20:25:11 +0530

    Create Range Frequency Query.cpp

---
+++ Day23/Range Frequency Query.cpp
@@ -0,0 +1,5 @@
+/*
+Platform : Leetcode
+Contest :- Leetcode Weekly contest 268
+Approach :- We need to store the index for each value and do binary search on them to find the amount of such values .
+*/
