97765b0580c0b5f15120e8cfe26ba2be775baac1
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-11-21 19:58:18 +0530

    Update Watering Plants.cpp

--- Day23/Watering Plants.cpp
+++ Day23/Watering Plants.cpp
@@ -4,3 +4,24 @@ Contest :- Leetcode Weekly contest 268
 Approach :- We will use bruteforce approach where we will fill our can with full capacity and water the plants if the amoount of water in can is less than the water plant require
             then we will go back to river and continue the process.
 */
+class Solution {
+public:
+    int wateringPlants(vector<int>& plants, int capacity) {
+        int ans=0,d=capacity;
+        
+        for(int i=0;i<plants.size();++i){
+            if(d<plants[i]){
+                d=capacity;
+                ans+=(2*i)+1;
+                d-=plants[i];
+            }
+            else{
+                d-=plants[i];
+                ans++;
+            }
+        }
+        
+        
+        return ans;
+    }
+};
