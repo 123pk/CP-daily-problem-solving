b56f327d70b29d139fad6e0b3ec206c0ef42ee66
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-11-21 19:51:03 +0530

    Create Two furthest houses with different color.cpp

---
+++ Day23/Two furthest houses with different color.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Leetcode
+Contest :- Leetcode Weekly contest 268
+Approach :- The basic approach is the max distance between first element and last element no equal to first element or last element and first element that is not equal to last eleemnt.
+*/
