6edd07b9869a93a4cbcd9aea4ec3e584f6efcb33
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-11-22 21:59:30 +0530

    Create A - Distance.cpp

---
+++ Day24/A - Distance.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Educational Round 117 Rated for Div 2
+Approach :- There are just three cases , if sum of (x,y) is odd then we don't have any solution , else if x==y then we can either put to {x,0} or {0,x} , else
+            if(x>y) then we have ((x+y)/2,0) else (0,(x+y)/2)
+*/
