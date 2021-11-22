967265b91b2840b34c9bdc5c8dc009c11021395f
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-11-22 22:03:39 +0530

    Create B - Special permuation.cpp

---
+++ Day24/B - Special permuation.cpp
@@ -0,0 +1,7 @@
+/*
+Platform :- Codeforces
+Contest :- Educational Round 118
+Approach :- first first the 'a' as first element and 'b' as first element of second half in array , use a map to only use unused values between 1 and n if we are not able to fill
+           any half with given constrants then we print -1 , else we start filling max values in first half going from 'n' to >'a'and from 1 to <b' in second half, all used exactly one
+           time
+*/
