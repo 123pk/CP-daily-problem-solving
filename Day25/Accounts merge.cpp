c36575164703fcefa26f6398fa248528f153a3a6
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-11-29 20:50:10 +0530

    Update Accounts merge.cpp

--- Day25/Accounts merge.cpp
+++ Day25/Accounts merge.cpp
@@ -2,3 +2,37 @@
 Platform :- Leetcode
 Approach :- Using union find we will be able to find all the disting disjoint set and store in answer where emails are in sorted order.
 */
+class Solution {
+public:
+    vector<vector<string>> accountsMerge(vector<vector<string>>& acts) {
+        map<string, string> owner;
+        map<string, string> parents;
+        map<string, set<string>> unions;
+        for (int i = 0; i < acts.size(); i++) {
+            for (int j = 1; j < acts[i].size(); j++) {
+                parents[acts[i][j]] = acts[i][j];
+                owner[acts[i][j]] = acts[i][0];
+            }
+        }
+        for (int i = 0; i < acts.size(); i++) {
+            string p = find(acts[i][1], parents);
+            for (int j = 2; j < acts[i].size(); j++)
+                parents[find(acts[i][j], parents)] = p;
+        }
+        for (int i = 0; i < acts.size(); i++)
+            for (int j = 1; j < acts[i].size(); j++)
+                unions[find(acts[i][j], parents)].insert(acts[i][j]);
+
+        vector<vector<string>> res;
+        for (pair<string, set<string>> p : unions) {
+            vector<string> emails(p.second.begin(), p.second.end());
+            emails.insert(emails.begin(), owner[p.first]);
+            res.push_back(emails);
+        }
+        return res;
+    }
+private:
+    string find(string s, map<string, string>& p) {
+        return p[s] == s ? s : find(p[s], p);
+    }
+};
