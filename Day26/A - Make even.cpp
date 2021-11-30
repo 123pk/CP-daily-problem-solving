6615bba3849b9373e1fc45bd22ed9bc1ecbaaac6
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-11-30 23:14:18 +0530

    Update A - Make even.cpp

--- Day26/A - Make even.cpp
+++ Day26/A - Make even.cpp
@@ -6,3 +6,31 @@ Contest :- If there is no even elemnt in our number then we cannot change it int
            Case 2 :- First character or digit of number if even then we just need one operation and last digit is swapped with first digit and we get even number
            Case 3 :- We need 2 operations , in operation one we make first element even then it becomes case 2.
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        string s;
+        cin>>s;
+        
+        int f=0;
+        for(auto &x:s){
+            if((x-'0')%2==0){
+                f=1;break;
+            }
+        }
+        
+        if(f==0)cout<<"-1\n";
+        else {
+            if((s[s.size()-1]-'0')%2==0)cout<<0<<"\n";
+            else{
+                if((s[0]-'0')%2==0)cout<<1<<"\n";
+                else cout<<2<<"\n";
+            }
+        }
+    }
+}
