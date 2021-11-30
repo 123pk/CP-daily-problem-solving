7e9eb7110cbbab60768248bb88de4ecd23ce6ce8
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-11-30 22:50:08 +0530

    Create A - Make even.cpp

---
+++ A - Make even.cpp
@@ -0,0 +1,8 @@
+/*
+Platform :- Codeforces
+Contest :- If there is no even elemnt in our number then we cannot change it into even
+           else we have three cases 
+           Case 1 :- Already even  --> we didnot need to perform any operation 
+           Case 2 :- First character or digit of number if even then we just need one operation and last digit is swapped with first digit and we get even number
+           Case 3 :- We need 2 operations , in operation one we make first element even then it becomes case 2.
+*/
