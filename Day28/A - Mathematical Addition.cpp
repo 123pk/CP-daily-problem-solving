7341392a4fe147dacb3b44243fbd05a972ef21e4
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-12-02 21:57:23 +0530

    Update A - Mathematical Addition.cpp

--- Day28/A - Mathematical Addition.cpp
+++ Day28/A - Mathematical Addition.cpp
@@ -3,3 +3,31 @@ Platform :- Codeforces
 Contest :- Codeforces Round 755 Div 2
 Approach :- Simplify the given expression and you will come to this final equation ( v^2 * x = - (u^2)*y) of  (x = -u^2)  and y = v^2
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        long long a,b;
+        cin>>a>>b;
+        
+        /*
+         x/a + y/b = (x+y)/(a+b)
+         
+         x/a  + y/b = (x)/(a+b) + (y/a+b)
+         
+        */
+        
+        long long x=-1;
+        x*=a;
+        x*=a;
+        
+        long long y=b*b;
+        cout<<x<<" "<<y<<"\n";
+        
+    }
+}
