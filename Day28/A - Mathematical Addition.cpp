42b3a0e5f3f81a6e8d60a00e2c642f4301e5a704
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-12-02 21:56:55 +0530

    Create A - Mathematical Addition.cpp

---
+++ Day28/A - Mathematical Addition.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Codeforces 
+Contest :- Codeforces Round 755 Div 2
+Approach :- Simplify the given expression and you will come to this final equation ( v^2 * x = - (u^2)*y) of  (x = -u^2)  and y = v^2
+*/
