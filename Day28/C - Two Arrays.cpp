3dfccf78a77a627790c43932e312323b90520aa8
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-12-02 21:59:06 +0530

    Create C - Two Arrays.cpp

---
+++ Day28/C - Two Arrays.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Round 755 Div 2
+Approach :- Just sort both the arrays if any element is not equal then if (B[i] - A[i] )!=1 the we cannot be able to make two array equal in one operation
+*/
