e350bde0a1627c7d708edeb173479bcec437f134
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-12-03 22:34:39 +0530

    Create B - Kalindrome Array.cpp

---
+++ Day29/B - Kalindrome Array.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Global Round 17
+Approach :- Since we only need to remove one element we will look for the two elements A[i] & A[n-1-i] where A[i]!=A[n-1-i] , if removing any of these doesnot give us a palindromic
+            array then we cannot create one else we can always create one .
+*/
