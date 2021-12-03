e7021afeac0451a50a7c4defbfd973b9b309eaab
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-12-03 21:46:18 +0530

    Create B- Triple metre.cpp

---
+++ Day29/B- Triple metre.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Atcoder
+Contest :- Atcoder Beginner Contest 230
+Approach :- first create a string of repeatative "oxx" of length greater than 10 , now check for given string in this string if present then it is possible else no.
+*/
