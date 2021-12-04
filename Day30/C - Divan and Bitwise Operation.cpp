380687235c432ca9a8161c91c58d03b1c3962109
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-12-04 22:15:05 +0530

    Create C - Divan and Bitwise Operation.cpp

---
+++ Day30/C - Divan and Bitwise Operation.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Contest 757 Div 2
+APPROACH :- Here we need to know some key points that are each bit of any element is set 2^n-1 times while finding XOR of all subsequences ,
+            so we will find OR of all the elements and sum of all XOR of all subesequence is ( OR_Value * 2^n-1 )% mod 
+*/
