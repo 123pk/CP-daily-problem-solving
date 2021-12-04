b7ca0352e8a36bcf0ac1098b7bf1d9692e928975
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-12-04 22:03:53 +0530

    Create A - Divian and Store.cpp

---
+++ Day30/A - Divian and Store.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Round 757 Div 2
+Approach :- Bruteforce it , sort the chocolates and chooose the chocolates which are not too cheap and neither too expensive and if you have the money to buy them then buy it.
+*/
