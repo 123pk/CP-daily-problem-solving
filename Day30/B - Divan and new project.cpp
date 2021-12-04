380007eda609c55cc5b4bf4b47c816f1f56fc1d6
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-12-04 22:11:35 +0530

    Create B - Divan and new project.cpp

---
+++ Day30/B - Divan and new project.cpp
@@ -0,0 +1,7 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Round 757 Div 2
+Approach :- Sort the given values and we want to keep those buildings near to the office which we need to visit most number of times or we can say the relation between distance
+            and the number of times we want to visit them are directly dependent. chhose any integer coordinate and set it to office now start making building either side of it 
+            chhosing the most visited building to be the first.
+*/
