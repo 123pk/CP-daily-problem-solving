fc0ceae893811c5dd6213cd47e833fbe73a3f507
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-12-05 21:42:45 +0530

    Update Find 3 digit even number.cpp

--- Day31/Find 3 digit even number.cpp
+++ Day31/Find 3 digit even number.cpp
@@ -1,6 +1,6 @@
 /*
 Platform :- Leetcode
-Contest :- Leetcode Weekly contest 
+Contest :- Leetcode Weekly contest 270
 Approach :- We store the frequency of each digit of array and now run a loop from 100 to 998 since we just need three digit even number if the frequency of digit of number can
            be satisifed and we have atleast that frequency of particular digits then we can create that number and add to our answer.
 */
