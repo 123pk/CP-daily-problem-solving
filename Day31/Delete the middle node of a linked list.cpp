ba83036cf9f930e73c4b845d975c3c1d44804bd0
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-12-05 21:38:02 +0530

    Create Delete the middle node of a linked list.cpp

---
+++ Day31/Delete the middle node of a linked list.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Leetcode
+Contest:- Leetcode Weekly contest 
+Approach :- first take all the elemnts of linked list and store in vector now remove the middle element and create a new linked list of those array of elements and return the head
+            of this new linked list
+*/
