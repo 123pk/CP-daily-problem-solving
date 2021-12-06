c57176cccad93da22c46495c1e7f357b6208bfb7
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-12-06 21:07:45 +0530

    Create D - Xenia and Bit Operations.cpp

---
+++ Day32/D - Xenia and Bit Operations.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Round 197 Div 2
+Approach :- The operations said in problem statement is suitable for tree operation , a perfect complete binary tree is formed if we will perform the operations , so we will create
+            the complete binary tree with operations and we will update leaf to root path which will take logn operations and overall time complexity of program is goint to ( m*log(n)) 
+*/
