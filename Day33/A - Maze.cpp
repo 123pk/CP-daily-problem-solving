26d8b021321e4044549d794394c49a91f198b08c
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-12-07 20:52:21 +0530

    Create A - Maze.cpp

---
+++ Day33/A - Maze.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Round 222 Div 2
+Approach :- We will do bfs as we have connected free cells we will look for (s-k) free cells and mark the remaining 'k' cells as we know remaining (s-k) cells are already connected
+*/
