9cdcc0bf4e5cdf46f06183a0605e78d11fdde817
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-12-10 21:58:04 +0530

    Update B - Team Composition: Programmers and Mathematicians.cpp

--- Day35/B - Team Composition: Programmers and Mathematicians.cpp
+++ Day35/B - Team Composition: Programmers and Mathematicians.cpp
@@ -3,3 +3,20 @@ Platform :- Codeforces
 Contest :- Codeforces Round 756 Div 3
 Approach :- The max number of teams can be created is (a+b)/4 , and min(a,b)
 */
+#include <iostream>
+
+using namespace std;
+
+int main() {
+     int t;
+     cin>>t;
+     
+     while(t--){
+         long long a,b;
+         cin>>a>>b;
+         
+         long long ans=(a+b)/4;
+         ans=min(ans,min(a,b));
+         cout<<ans<<"\n";
+     }
+}
