bbf1b943d20777a589e5ccf851c0ac8795c4829e
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-12-10 20:57:19 +0530

    Create Jump Game III.cpp

---
+++ Day35/Jump Game III.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Leetcode
+Approach :- We will do dfs from start and maintain a visited array so that we donot visit any node multiple times , and for each index we will look for "left" and "right" index 
+            we can reach from current index and do dfs on them.
+*/
