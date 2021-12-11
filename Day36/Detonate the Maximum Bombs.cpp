3377587c3fa7c432cab9471d550eb7511b8f92c2
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-12-11 21:36:10 +0530

    Create Detonate the Maximum Bombs.cpp

---
+++ Day36/Detonate the Maximum Bombs.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Leetcode
+Contest :- Leetcode Bi Weekly contest 67
+Approach :- We will look for index which will be diffused if we diffuse any bomb , it will create  a disjoint set and answer is the maximum no of element in any Disjoint set
+*/
