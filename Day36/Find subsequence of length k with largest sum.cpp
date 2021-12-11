089d3c1de4b5fec3aa5e0a481198015fb7a4db45
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-12-11 21:31:11 +0530

    Create Find subsequence of length k with largest sum.cpp

---
+++ Day36/Find subsequence of length k with largest sum.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Leetcode
+Contest :- Leetcode Bi weekly contest 67
+Approach :- Since we need to choose subsequence we will simply choose the largest k elements of array in ascending order of index
+*/
