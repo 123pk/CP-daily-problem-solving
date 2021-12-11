95a6ebcc5c0be4d936786d6356770c471b6f74da
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-12-11 21:34:11 +0530

    Create Find Good Days to Rob the Bank.cpp

---
+++ Day36/Find Good Days to Rob the Bank.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Leetcode
+Contest :- Leetcode Weekly contest 67
+Approach :- We will create two array one which store the prefix array of no of continuous days of a non increasing continuous segment for each  index , we will choose those index
+            which are having value in prefix and suffix array greater than "time".
+*/
