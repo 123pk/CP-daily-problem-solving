7bef6cd9029c1c7fa19deb5cd8e6af2178c833d7
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-12-12 22:47:46 +0530

    Update A - Life of flowerr.cpp

--- Day37/A - Life of flowerr.cpp
+++ Day37/A - Life of flowerr.cpp
@@ -1,4 +1,5 @@
 /*
 Platform :- Codeforces
 Contest :- Codeforces Round 759
+Approach :- If there are two continuous days with no watering then answer is "-1" else if there are more than 1 days of continuous watering days then we add "5" to height else 1
 */
