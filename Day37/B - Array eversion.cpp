a35e977f34069038a8c05a5c070c936090e73ce7
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-12-12 22:49:47 +0530

    Update B - Array eversion.cpp

--- Day37/B - Array eversion.cpp
+++ Day37/B - Array eversion.cpp
@@ -3,3 +3,37 @@ Platform :- Codeforces
 Contest :- Codeforces Round 759 
 Approach :- Here if the max element comest to the end then we start getting same sequence , so we will look for lenght of increasing subsequnce from behind and ans is len-1 .
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        int n;
+        cin>>n;
+        
+        long long mx=0;
+        long long A[n];
+        for(int i=0;i<n;++i){
+            cin>>A[i];
+            mx=max(mx,A[i]);
+        }
+        
+        if(A[n-1]==mx)cout<<"0\n";
+        else{
+            long long prev=A[n-1];
+            int ans=0;
+            for(int i=n-1;i>=0;--i){
+                if(A[i]>prev){
+                    prev=A[i];
+                    ans++;
+                }
+            }
+            cout<<ans<<"\n";
+        }
+        
+        
+    }
+}
