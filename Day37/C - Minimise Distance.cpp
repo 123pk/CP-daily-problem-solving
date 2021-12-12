9b188a9e5c976b220da0ae44f2486d842b6388ff
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-12-12 22:51:16 +0530

    Update C - Minimise Distance.cpp

--- Day37/C - Minimise Distance.cpp
+++ Day37/C - Minimise Distance.cpp
@@ -1,5 +1,6 @@
 /*
 Platform :- Codeforces
 Contest :- Codeforces Round 759 
-Approach :- 
+Approach :- Approach create two different arrays to deal with positive and -ve numbers , now sort the values and choose group of 'k' , one with max value is visited last and other
+            are visited in any order.
 */
