8004a339c9eb1bd51ca9b64a16597cae5f03dafe
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-12-12 22:49:22 +0530

    Update B - Array eversion.cpp

--- Day37/B - Array eversion.cpp
+++ Day37/B - Array eversion.cpp
@@ -1,5 +1,5 @@
 /*
 Platform :- Codeforces
 Contest :- Codeforces Round 759 
-Approach :- 
+Approach :- Here if the max element comest to the end then we start getting same sequence , so we will look for lenght of increasing subsequnce from behind and ans is len-1 .
 */
