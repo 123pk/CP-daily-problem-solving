e8f55b2b77d327aee81149bd8dd17b620c1fde43
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-12-12 22:44:48 +0530

    Create Watering Plants II.cpp

---
+++ Day37/Watering Plants II.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Leetcode
+Contest :- Leetcode Weekly contest 271
+Approach :- Use two pointers one from start and one from end and start watering plants if the can has less water than required then fill the can and update answer and continue.
+*/
