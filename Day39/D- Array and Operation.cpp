154a0f3f319b618b87e567739d39e172e6897528
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-12-14 23:40:51 +0530

    Create D- Array and Operation.cpp

---
+++ Day39/D- Array and Operation.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Round 760 Div 3
+Approach :- Sort the array and since we need to minimise the sum , then we choose the last (n-(2*k)) elements as they contribute to the largest part of sum , we choose (n-2k,n-k),
+           (n-2k+1,n-k+1)...and so on . This will help us in acheiving the desired sum.
+*/
