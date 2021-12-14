90c268d4a3b4a797e2444d9e423e98b94b538bb3
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-12-14 23:24:31 +0530

    Create A - Polycrap and sum of subsequence.cpp

---
+++ Day39/A - Polycrap and sum of subsequence.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Round 760 Div 3
+Approach :- Sort the array , the two smallest element are already in main array we cannot generate them , now out of 3rd or 4 element we have to choose any one as our final element
+            if sum of first two element > 3rd element then we choose 3rd element else we choose 4th one.
+*/
