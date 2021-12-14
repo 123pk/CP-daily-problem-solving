28747dba46d80d6e7738dd94d4be42ea3119a7f8
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-12-14 23:32:51 +0530

    Create C - Paint the array.cpp

---
+++ Day39/C - Paint the array.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Round 760 Div 3
+Approach :- Since the elements at odd position shoudl be colored in same color and even position in same color and both colors need to be differnt , so we compute GCD of even index
+           and odd index elements , if both of them is 1 then we cannot choose any such value , else we choose the values and check if any of them satisfy the condition or not .
+*/
