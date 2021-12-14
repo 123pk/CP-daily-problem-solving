5029e09ab99e21119e1eccd81c0d473117c81e90
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-12-15 00:00:17 +0530

    Update E - Siger's tour.cpp

--- Day39/E - Siger's tour.cpp
+++ Day39/E - Siger's tour.cpp
@@ -13,3 +13,70 @@ Approach :- You need to simplify and you will find that for given 'n' , the sum 
            we know (x0 + x1 + x2  = tot_sum / z)
            and simplify using these two equations .
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+         
+        
+        long long A[n];
+        long long tot=0;
+        long long mn=(n*(n+1))/2;
+        long long z=n;
+        z*=mn;
+        for(int i=0;i<n;++i){
+            cin>>A[i];
+            tot+=A[i];
+        }
+        
+        if(tot%mn)cout<<"NO\n";
+        else{
+            
+            int f=0;
+            vector<long long>ans;
+            //we start simpifying
+            long long sum = tot/mn;
+            
+            for(int i=0;i<n;++i){
+                if(i==0){
+                     
+                    long long val = (A[i]-sum);
+                   
+                    val = (A[n-1]-val);
+                    
+                    if(val%n || val<=0){
+                      
+                        f=1;break;
+                    }
+                    val/=n;
+                    ans.push_back(val);
+                }
+                else{
+                    long long val = (A[i]-sum);
+                    val = (A[i-1]-val);
+                    if(val%n || val<=0){
+                       
+                        f=1;break;
+                    }
+                    val/=n;
+                    ans.push_back(val);
+                }
+            }
+            
+            
+            if(f)cout<<"NO\n";
+            else{
+                cout<<"YES\n";
+                for(auto &x:ans)cout<<x<<" ";
+                cout<<"\n";
+            }
+            
+        }
+        
+    }
+}
