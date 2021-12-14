13e859303757c9f7d77c8763374777fbac6be47d
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-12-14 23:29:27 +0530

    Create B - Missing Bigram.cpp

---
+++ Day39/B - Missing Bigram.cpp
@@ -0,0 +1,7 @@
+/*
+Platform :- Codeforces
+Contet :- Codeforces Round 760 Div 3
+Approach :- the last character of previous string should be same else there is missing string or one which is removed , we always add the first string to our answer , if we found any
+            index where the condition is not satisfied then we add whole string else the last element to our answer.
+            Now if the size of string is less than 'n' then we will add the first element remainig number of times in start of string.
+*/
