38148e5dd5aa2d1177f728faab7b68325984a48f
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-12-17 21:40:16 +0530

    Update Minimum height tree.cpp

--- Day41/Minimum height tree.cpp
+++ Day41/Minimum height tree.cpp
@@ -1,4 +1,39 @@
 /*
 Platform :- Leetcode
-Approach :- 
+Approach :- BFS
 */
+class Solution {
+ public:
+  vector<int> findMinHeightTrees(int n, vector<pair<int, int>>& edges) {
+    // Initialize the undirected graph
+    vector<unordered_set<int>> adj(n);
+    for (pair<int, int> p : edges) {
+      adj[p.first].insert(p.second);
+      adj[p.second].insert(p.first);
+    }
+    // Corner case
+    vector<int> current;
+    if (n == 1) {
+      current.push_back(0);
+      return current;
+    }
+    // Create first leaf layer
+    for (int i = 0; i < adj.size(); ++i) {
+      if (adj[i].size() == 1) {
+        current.push_back(i);
+      }
+    }
+    // BFS the graph
+    while (true) {
+      vector<int> next;
+      for (int node : current) {
+        for (int neighbor : adj[node]) {
+          adj[neighbor].erase(node);
+          if (adj[neighbor].size() == 1) next.push_back(neighbor);
+        }
+      }
+      if (next.empty()) return current;
+      current = next;
+    }
+  }
+};
