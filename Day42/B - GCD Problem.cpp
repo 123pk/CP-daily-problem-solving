660d1ba0d512b66dc35616004408416d912b35c0
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-12-17 20:57:00 +0530

    Update B - GCD Problem.cpp

--- Day42/B - GCD Problem.cpp
+++ Day42/B - GCD Problem.cpp
@@ -8,3 +8,29 @@ Approach :- there are two cases
            
            Case 2 :- N is even then we have ( N/2 , N/2-1 , 1 ) as gcd of odd and even is 1 so this is correct .
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+      
+        
+        long long n;
+        cin>>n;
+        
+        if(n%2){
+            long long val = n/2;
+            if(val%2){
+                cout<<(val-2)<<" "<<val+2<<" "<<1<<"\n";
+            }
+            else{
+                cout<<(val-1)<<" "<<(val+1)<<" "<<1<<"\n";
+            }
+        }
+        else cout<<(n/2)<<" "<<((n/2)-1)<<" "<<1<<"\n";
+        
+    }
+}
