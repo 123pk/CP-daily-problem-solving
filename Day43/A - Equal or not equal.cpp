43a7ddfabf6c802838c40997e4bd3db052850c25
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-12-18 23:05:34 +0530

    Update A - Equal or not equal.cpp

--- Day43/A - Equal or not equal.cpp
+++ Day43/A - Equal or not equal.cpp
@@ -2,3 +2,26 @@
 Platform :- Codeforces
 Contest :- Codeforces Educational Round 119
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        string s;
+        cin>>s;
+        
+        int n = s.size();
+        
+        // if there is just 1 N then it is impossible else we always have one array which satisfy the condition
+        int c=0;
+        for(int i=0;i<(n);++i){
+            if(s[i]=='N')c++;
+        }
+        
+        if(c==1)cout<<"NO\n";
+        else cout<<"YES\n";
+    }
+}
