17aeb78131d160d1e3c5f29facabac8b7a424f55
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-12-19 19:59:51 +0530

    Update Number of smoothest descent period of stock.cpp

--- Day44/Number of smoothest descent period of stock.cpp
+++ Day44/Number of smoothest descent period of stock.cpp
@@ -4,3 +4,33 @@ Contest :- Leetcode Weekly contest 272
 Approach :- For a particular subarray of size 'x' number of such descents is (x*(x+1))/2 , so we traverse from 0 to n-1 and find those subarray and add the following values
             to our answer.
 */
+class Solution {
+public:
+    long long getDescentPeriods(vector<int>& prices) {
+        long long ans=0;
+        long long c=0;
+        for(int i=0;i<prices.size();++i){
+            if(i==0){
+                c++;
+                if(i==prices.size()-1){
+                    ans+=(c*(c+1))/2;
+                }
+                
+            }
+            else{
+                if((prices[i-1]-prices[i])==1){
+                    c++;
+                }
+                else{
+                    ans+=(c*(c+1))/2;
+                    c=1;
+                }
+                
+                if(i==prices.size()-1){
+                    ans+=(c*(c+1))/2;
+                }
+            }
+        }
+        return ans;
+    }
+};
