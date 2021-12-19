0238af8367e9fc22313c427362b188f856219264
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-12-19 19:59:24 +0530

    Create Number of smoothest descent period of stock.cpp

---
+++ Day44/Number of smoothest descent period of stock.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Leetcode
+Contest :- Leetcode Weekly contest 272
+Approach :- For a particular subarray of size 'x' number of such descents is (x*(x+1))/2 , so we traverse from 0 to n-1 and find those subarray and add the following values
+            to our answer.
+*/
