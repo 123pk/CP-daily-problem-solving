93ddbe4f7c7236f722e1341c16f2f9143ae0d459
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-12-19 23:01:54 +0530

    Update Musical chairs.cpp

--- Day44/Musical chairs.cpp
+++ Day44/Musical chairs.cpp
@@ -3,3 +3,27 @@ Platform :- Codechef
 Contest :- Codeforces Cook off December 2021
 Approach :- After the contest 
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+   int t;
+   cin>>t;
+   
+   while(t--){
+       long long n;
+       cin>>n;
+       n--;
+       
+       long long ans=0;
+       for(long long i=1;i*i<=n;++i){
+           if(n%i==0){
+               if(n/i==i){
+                   ans++;
+               }
+               else ans+=2;
+           }
+       }
+       cout<<ans<<"\n";
+   }
+}
