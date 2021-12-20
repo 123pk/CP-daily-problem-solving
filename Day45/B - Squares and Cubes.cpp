5a9106313a9a753288a226aa1fd6d3288eb912f9
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-12-20 22:31:53 +0530

    Create B - Squares and Cubes.cpp

---
+++ Day45/B - Squares and Cubes.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Round 762 Div 3
+Approach :- We calculate the cubes and square root until they are less than n and insert in set and set size is our answer.
+*/
