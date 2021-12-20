3dcd54a791cb49b1c9739879fd5add0738e35a8d
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-12-20 22:30:26 +0530

    Update A - Square string?.cpp

--- Day45/A - Square string?.cpp
+++ Day45/A - Square string?.cpp
@@ -3,3 +3,31 @@ Platform :- Codeforces
 Contest :- Codeforces Round 762 Div3
 Approach :- if the length is odd then the string is not square else we compare the two halfs of string .
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        string s;
+        cin>>s;
+        
+        int n = s.size();
+        if(n%2)cout<<"NO\n";
+        else{
+            int z = (n/2);
+            int f=0;
+            
+            for(int i=0;i<(n/2);++i){
+                if(s[i]!=s[z+i]){
+                    f=1;break;
+                }
+            }
+            
+            if(f)cout<<"NO\n";
+            else cout<<"YES\n";
+        }
+    }
+}
