103d43da9f1789ec995c3485ee54b53dc5481480
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-12-20 22:34:20 +0530

    Create C - Wrong addition.cpp

---
+++ Day45/C - Wrong addition.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces ROund 762 Div 3
+Approach :- We need to care of few case and greedily implement , if the digit of 'A' is greater than the digit of 'S' then we have to use two digits of 's' and if we have remaining
+            digits in S then we add as it is to our answer.
+*/
