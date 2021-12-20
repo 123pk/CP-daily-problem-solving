cd744a342f1277c932f5b66550dc891285ed355d
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-12-20 22:32:05 +0530

    Update B - Squares and Cubes.cpp

--- Day45/B - Squares and Cubes.cpp
+++ Day45/B - Squares and Cubes.cpp
@@ -3,3 +3,33 @@ Platform :- Codeforces
 Contest :- Codeforces Round 762 Div 3
 Approach :- We calculate the cubes and square root until they are less than n and insert in set and set size is our answer.
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        long long n;
+        cin>>n;
+        
+        //check the squres
+        set<long long>value;
+        for(long long i=1;i*i<=n;++i){
+            value.insert(i*i);
+        }
+        
+        long long z=2,d=0;
+        while(1){
+            d = (z*z*z);
+            if(d<=n){
+                value.insert(d);
+            }
+            else break;
+            z++;
+        }
+        
+        cout<<value.size()<<"\n";
+    }
+}
