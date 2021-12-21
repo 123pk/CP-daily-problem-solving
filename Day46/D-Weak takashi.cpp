e92367b52257e22a26b226272546672db568585c
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-12-21 21:59:18 +0530

    Update D-Weak takashi.cpp

--- Day46/D-Weak takashi.cpp
+++ Day46/D-Weak takashi.cpp
@@ -1,1 +1,5 @@
-
+/*
+Platfor :- Atcoder
+Contest :- Atcoder Beginner Contest 232
+Approach :- DP of n*m where dp[i] stores the no of ways to reach there and answer is max value of all . dp[i][j] = max(dp[i-1][j],dp[i][j-1]) + 1 for valid cells 
+*/
