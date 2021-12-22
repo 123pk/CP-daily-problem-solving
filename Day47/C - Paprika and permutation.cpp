da930c693c20c4e1d35fbd8b020ffaf672cb84d6
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-12-22 23:03:19 +0530

    Update C - Paprika and permutation.cpp

--- Day47/C - Paprika and permutation.cpp
+++ Day47/C - Paprika and permutation.cpp
@@ -1,1 +1,7 @@
-
+/*
+Platform :- Codeforces
+Contest :- Codeforces Round 761 Div 2
+Approach :- we run a loop from 1 to n and mark all those which are present in array and we now try to change the remaining values after sorting in increasing order to
+            ith with the mod operation , we can do so if the 
+            z = (value+1)/2 , if (z-1) is < i then we cannot covert it to that index .else we increment our count. 
+*/
