d3571f7236e8d038dde650fd91f6ada3cf05be5e
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-12-25 19:29:18 +0530

    Create D - Count Intervals.cpp

---
+++ Day50/D - Count Intervals.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Atcoder
+Contest :- Atcoder Beginner contest 233
+Approach :- It is same as standard problem where we need to find number of subarrays with sum equal to 'k' , we basically use two pointer to find it in O(n)
+*/
