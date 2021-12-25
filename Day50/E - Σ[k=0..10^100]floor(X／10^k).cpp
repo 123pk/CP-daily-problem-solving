f411c2f8c97b15edc0cd54663e7e4054d83eace0
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-12-25 19:36:25 +0530

    Update E - Σ[k=0..10^100]floor(X／10^k).cpp

--- Day50/E - Σ[k=0..10^100]floor(X／10^k).cpp
+++ Day50/E - Σ[k=0..10^100]floor(X／10^k).cpp
@@ -12,3 +12,34 @@ Approach :- If you will carefully analyse the problem you will find that
             You can observe that last digit of our value is sum of all the (digits % 10), 
             then second last digit is ( sum of first n-1 digits + carry ) %10..and so on
 */
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    cin>>s;
+    
+    long long tot=0;
+    for(auto &x:s){
+        tot+=(x-'0');
+    }
+    int j=s.size()-1;
+    string ans="";
+    long long val=0;
+    long long carry=0;
+    for(int i=0;i<s.size();++i){
+        val=tot+carry;
+        ans+=to_string(val%10);
+        carry=val/10;
+        tot-=(s[j]-'0');
+        j--;
+    }
+    if(carry){
+        ans+=to_string(carry);
+    }
+    
+    reverse(ans.begin(),ans.end());
+    
+    cout<<ans<<"\n";
+}
