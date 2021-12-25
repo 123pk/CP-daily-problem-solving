a5886d576d12cda1e638fd9d0a98a1739b488fe7
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-12-25 19:17:52 +0530

    Update B - And it's non zero.cpp

--- Day49/B - And it's non zero.cpp
+++ Day49/B - And it's non zero.cpp
@@ -1,5 +1,6 @@
 /*
 Platform :- Codeforces
 Contest :- Codeforces Global Round 18
-Approach :- 
+Approach :- Pre compute prefix sum of count of binary bits , for all values from 1 to 200000 , you can store details of 32 bits it is sufficient for following constraints
+            now for each l and r find the count of bits in range (r-(l+1)) ,  the ( (r-(l+1))-max_count )is our answer
 */
