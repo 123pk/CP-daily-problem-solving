efcf962d31397b735be766bf01dde31b9d79dc6d
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-12-26 18:42:51 +0530

    Update Execution of all suffix instructions staying in grid.cpp

--- Day51/Execution of all suffix instructions staying in grid.cpp
+++ Day51/Execution of all suffix instructions staying in grid.cpp
@@ -3,3 +3,48 @@ Platform :- Leetcode
 Contest :- Leetcode Weekly contest 273
 Approach :- Do Bruteforce and for each value find the max no of operations that can be performed
 */
+class Solution {
+public:
+    vector<int> executeInstructions(int n, vector<int>& x, string s) {
+       int m = s.length();
+        vector<int>ans(m);
+        set<pair<int,int>>vis;
+        int d=0;
+        for(int i=0;i<m;++i){
+            //start from start pos
+            int start=x[0],end=x[1];
+            vis.clear();
+            vis.insert({x[0],x[1]});
+            int j=i;
+            d=0;
+         //   cout<<(i+1)<<"\n";
+            while(j<m){
+                if(s[j]=='L'){
+                    if((end-1)<0)break;
+                    d++;
+                    end--;
+                }
+                else if(s[j]=='R'){
+                    if((end+1)>=n)break;
+                    end++;
+                    d++;
+                }
+                else if(s[j]=='D'){
+                    if((start+1)>=n)break;
+                    d++;
+                    start++;
+                }
+                else{
+                    if((start-1)<0)break;
+                    d++;
+                    start--;
+                }
+                j++;
+               // cout<<start<<" "<<end<<"\n";
+            }
+            ans[i]=d;
+        }
+        
+        return ans;
+    }
+};
