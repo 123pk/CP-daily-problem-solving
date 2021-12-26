efe3a4efd3991ea9d7b632dd35d3d0065ec9ef44
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-12-26 18:32:42 +0530

    Update A number after doubel reversal.cpp

--- Day51/A number after doubel reversal.cpp
+++ Day51/A number after doubel reversal.cpp
@@ -3,3 +3,10 @@ Platform :- Leetcode
 Contest :- Leetcode Weekly contest 273
 Approach :- If the last digit is non zero then we will get the same number else we will get something else.
 */
+class Solution {
+public:
+    bool isSameAfterReversals(int num) {
+        if(num==0)return 1;
+        return num%10;
+    }
+};
