de9aadb4883d15d734d2489811be2d62e40b7785
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-12-28 21:40:24 +0530

    Update A - Robot cleaner.cpp

--- Day53/A - Robot cleaner.cpp
+++ Day53/A - Robot cleaner.cpp
@@ -3,3 +3,41 @@ Platform :- Codeforces
 Contest :- Codeforces Round 763 Div 2
 Approach :- Bruteforce the operations .
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        int n,m,x,y,a,b;
+        cin>>n>>m>>x>>y>>a>>b;
+        
+        int ans=0;
+        int f=0;
+        int d1=1,d2=1;
+        while(1){
+            if(x==n&&y==m){
+              ans+=(min(n-a,m-b));
+              break;
+            }
+            else{
+                if(x==a||y==b)break;
+                if(x==n){
+                    d1=-1;
+                }
+                else if(y==m){
+                    d2=-1;
+                }
+                x+=d1;
+                y+=d2;
+            }
+            ans++;
+            //cout<<x<<" "<<y<<" "<<d1<<" "<<d2<<" "<<ans<<"\n";
+        }
+        
+        cout<<ans<<"\n";
+        
+    }
+}
