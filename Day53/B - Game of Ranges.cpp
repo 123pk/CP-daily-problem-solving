3f328bff4a990a438043ba0f38dd3d2cf8eabcde
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-12-28 21:44:16 +0530

    Update B - Game of Ranges.cpp

--- Day53/B - Game of Ranges.cpp
+++ Day53/B - Game of Ranges.cpp
@@ -1,5 +1,6 @@
 /*
 Platform :- Codeforces
 Contest :- Codeforces Round 763 Div 2
-
+Approach :- We need to find count of ranges of all the values from ( 1 to n ) , and sort them in increasing order and  we choose the value with min count every time and find the 
+            appropriate range.
 */
