6a87670f7656a9585dff149f893cde5747d550bf
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-12-29 23:13:59 +0530

    Update A - Integer Diversity.cpp

--- Day54/A - Integer Diversity.cpp
+++ Day54/A - Integer Diversity.cpp
@@ -1,1 +1,6 @@
-
+/*
+Platform :- Codeforces
+Contest :- Good Bye 2021: 2022 is NEAR
+Approach :- Find the frequency of each element if some element is repeating then we change to negate of it if the negate is note present and at the end the number of
+            of unique values is our answer.
+*/
