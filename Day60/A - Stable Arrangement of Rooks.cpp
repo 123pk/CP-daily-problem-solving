d88ad04bd1191a3fd77eee741f97181694335d95
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-01-03 22:23:28 +0530

    Update A - Stable Arrangement of Rooks.cpp

--- Day60/A - Stable Arrangement of Rooks.cpp
+++ Day60/A - Stable Arrangement of Rooks.cpp
@@ -4,3 +4,36 @@ Platform :- Codeforces
 Contest :- Codeforces Hello 2022
 Approach :- we can place atmax (n+1)/2 rookie on the board . if there are more than it then it is not possible . it is always optimal to place on main diagonal and keeping a gap of 1
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        int n,k;
+        cin>>n>>k;
+        
+        int val =(n+1)/2;
+        if(val<k)cout<<"-1\n";
+        else{
+            for(int i=0;i<n;++i){
+                for(int j=0;j<n;++j){
+                    if(i==j){
+                        if(i%2==0){
+                            if(k){
+                                cout<<"R";
+                                k--;
+                            }
+                            else cout<<".";
+                        }
+                        else cout<<".";
+                    }
+                    else cout<<".";
+                }
+                cout<<"\n";
+            }
+        }
+    }
+}
