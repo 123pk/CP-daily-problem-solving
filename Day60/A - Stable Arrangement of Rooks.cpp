db08a433c8ac29e27a687184468aef249897fcdb
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-01-03 22:23:12 +0530

    Update A - Stable Arrangement of Rooks.cpp

--- Day60/A - Stable Arrangement of Rooks.cpp
+++ Day60/A - Stable Arrangement of Rooks.cpp
@@ -1,1 +1,6 @@
 
+/*
+Platform :- Codeforces
+Contest :- Codeforces Hello 2022
+Approach :- we can place atmax (n+1)/2 rookie on the board . if there are more than it then it is not possible . it is always optimal to place on main diagonal and keeping a gap of 1
+*/
