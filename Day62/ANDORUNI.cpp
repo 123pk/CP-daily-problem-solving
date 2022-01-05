fa2bb30cffddefab98989bb5468dbd5b89ac93dd
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-01-05 23:32:58 +0530

    Update ANDORUNI.cpp

--- Day62/ANDORUNI.cpp
+++ Day62/ANDORUNI.cpp
@@ -4,3 +4,19 @@ Platform :- Codechef
 Contest :- Codechef starters 21 
 Approach :- If sum of x and y is even then we need 0 coin else we need 1 coin.
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        int n,x,y;
+        cin>>n>>x>>y;
+        
+        int ans=(x+y)%2;
+        
+        cout<<ans<<"\n";
+    }
+}
