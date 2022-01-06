0038fb159124695376489e893b8b7e68fc1c1680
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-01-06 22:38:23 +0530

    Update 1094. Car Pooling.cpp

--- Day63/1094. Car Pooling.cpp
+++ Day63/1094. Car Pooling.cpp
@@ -3,3 +3,19 @@ Platform :- Leetcode
 Approach :- We maintain a frequency where we add capacity[i] passengers on start of journey and remove capacity[i] from end of journy then we find prefix sum and if at any point
             the sum is >capacity then we cannot fulfill the condition and return false else return true at the end.
 */
+class Solution {
+public:
+    bool carPooling(vector<vector<int>>& trips, int capacity) {
+        vector<int>pick(2000);
+        for(int i=0;i<trips.size();++i){
+            pick[trips[i][1]]+=trips[i][0];
+            pick[trips[i][2]]-=trips[i][0];
+        }
+        
+        for(int i=0;i<1003;i++){
+            if(i)pick[i]+=pick[i-1];
+            if(pick[i]>capacity)return false;
+        }
+        return true;
+    }
+};
