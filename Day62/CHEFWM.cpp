3c6b5a5d4694ca8e72710031918d1476a88ece31
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-01-06 18:33:07 +0530

    Update CHEFWM.cpp

--- Day62/CHEFWM.cpp
+++ Day62/CHEFWM.cpp
@@ -1,1 +1,5 @@
-
+/*
+Platform :- Codechef
+Contest :- Codechef starters 21
+Approach :- Count no of prime factors of 'm' let it be 'x' and store the factors of 'n' we our answer is the biggest factor of 'n' which is also <= 'x' .  
+*/
