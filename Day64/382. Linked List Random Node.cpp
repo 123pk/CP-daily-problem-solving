d2d21e4044eee3cc4245c50432d8a2f89323ec88
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-01-07 17:38:11 +0530

    Create 382. Linked List Random Node.cpp

---
+++ Day64/382. Linked List Random Node.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Leetcode
+Approach :- In c++ we have a function called "rand()" which generates random values from [0 to RAND_MAX ] range which is upto 32567 or so , we can create an array and store all
+            the values of linked list and let m = 'size of array'  , and we chooose index as ( rand()%m ) as this will generate values in range [0,m) and return value at that index 
+*/
