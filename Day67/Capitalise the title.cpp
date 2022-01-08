5efffbe0a7d3a18923158d68b20df59ac5556fdc
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-01-08 22:22:19 +0530

    Create Capitalise the title.cpp

---
+++ Day67/Capitalise the title.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Leetcode
+Contest :- Leetcode BiWeekly contest 69
+Approach :- First find all the stirg perform the operation and return the answer after cummulating both of them.
+*/
