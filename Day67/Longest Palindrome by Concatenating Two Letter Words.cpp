a6f4e7e7ed108036745214dc48aa69a213da14ae
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-01-08 22:16:00 +0530

    Create Longest Palindrome by Concatenating Two Letter Words.cpp

---
+++ Day67/Longest Palindrome by Concatenating Two Letter Words.cpp
@@ -0,0 +1,7 @@
+/*
+Platform :- Leetcode
+Contest :- leetcode Bi weekly contest 69
+Approach :- There can be two type of string 
+          1. both characters are equal , here we use even pairs and add to the string and at the end if we have multiple string with at least one occurence we use any one of them
+          2. characters are not equal here we use even pairs only .
+*/
