3b9521887fc27fae1057d7b7fddc6a36dd487cff
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-01-09 20:01:38 +0530

    Update Check if Every Row and Column Contains All Numbers.cpp

--- Day68/Check if Every Row and Column Contains All Numbers.cpp
+++ Day68/Check if Every Row and Column Contains All Numbers.cpp
@@ -2,3 +2,32 @@
 Platform :- Leetcode
 Approach:- Find all the values in row and store in set if set size != n return false do this for all rows then for all colummns
 */
+class Solution {
+public:
+    bool checkValid(vector<vector<int>>& matrix) {
+        
+        int n=matrix.size();
+        set<int>temp;
+      //  cout<<n<<"\n";
+        for(int i=0;i<n;++i){
+            temp.clear();
+            for(int j=0;j<n;++j){
+             ///   cout<<matrix[i][j]<<" ";
+                temp.insert(matrix[i][j]);
+            }
+           // cout<<"\n";
+            if(temp.size()<n)return false;
+        }
+        
+        for(int j=0;j<n;++j){
+            temp.clear();
+            for(int i=0;i<n;++i){
+                temp.insert(matrix[i][j]);
+            }
+            if(temp.size()<n)return false;
+        }
+        
+        
+        return true;
+    }
+};
