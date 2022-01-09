6f3c7551135190b0c2bf05c53c2a59f1cb2229b4
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-01-09 20:06:56 +0530

    Update Check if Every Row and Column Contains All Numbers.cpp

--- Day68/Check if Every Row and Column Contains All Numbers.cpp
+++ Day68/Check if Every Row and Column Contains All Numbers.cpp
@@ -1,5 +1,6 @@
 /*
 Platform :- Leetcode
+Contest :- Weekly contest 275
 Approach:- Find all the values in row and store in set if set size != n return false do this for all rows then for all colummns
 */
 class Solution {
