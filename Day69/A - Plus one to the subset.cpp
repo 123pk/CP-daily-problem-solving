a88933b756c5de991ece69ffc0500010a243641a
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-01-10 22:43:30 +0530

    Update A - Plus one to the subset.cpp

--- Day69/A - Plus one to the subset.cpp
+++ Day69/A - Plus one to the subset.cpp
@@ -3,3 +3,24 @@ Platform :- Codeforces
 Contest :- Codeforces Round 764 Div 3
 Approach :- Since we can only increment hence we nee to covert all value to the largest value and max no of operation required is [max_elemetn - min_element ] 
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        int n;
+        cin>>n;
+        
+        long long A[n];
+        for(int i=0;i<n;++i){
+            cin>>A[i];
+        }
+        sort(A,A+n);
+        long long ans=0;
+        ans=A[n-1]-A[0];
+        cout<<ans<<"\n";
+    }
+}
