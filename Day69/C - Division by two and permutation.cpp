6a9054ab8fb81a100410f7e8055cc7279bcd6664
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-01-10 22:57:02 +0530

    Create C - Division by two and permutation.cpp

---
+++ Day69/C - Division by two and permutation.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Round 764 Div 3
+Approach :- First if there are element in array which are also in permutation leave them and mark them so that we will not change them, for rest of elements we divide by 2 and 
+            if any unused elemment of permuataion is formed we mark it and if at the end all the elements are marked then we print yes else no
+*/
