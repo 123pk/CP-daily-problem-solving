1a3a5da241d7daeaf969d93d391d5db87ec3a52e
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-01-10 22:43:09 +0530

    Create A - Plus one to the subset.cpp

---
+++ Day69/A - Plus one to the subset.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Round 764 Div 3
+Approach :- Since we can only increment hence we nee to covert all value to the largest value and max no of operation required is [max_elemetn - min_element ] 
+*/
