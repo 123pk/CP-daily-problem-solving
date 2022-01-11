ac6d384530c6e2cd1c96abd0efb039ec6d85c92c
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-01-11 22:30:31 +0530

    Update D - Palindrome coloring.cpp

--- Day70/D - Palindrome coloring.cpp
+++ Day70/D - Palindrome coloring.cpp
@@ -7,3 +7,59 @@ Approach :- Find no of pairs of equal characters we can make , take count of uni
             if we want 'k' palindrome strings then we need to distribute x/k to each of 'k' .
             then there would be some extraa values if (x%k) if (x%k)*2+y>=k then d=1 and max lenth is (x/k)*2+d
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+         int n,k;
+         cin>>n>>k;
+         
+         
+         string s;
+         cin>>s;
+         
+         vector<int>z(26);
+         for(int i=0;i<n;++i){
+             z[s[i]-'a']++;
+         }
+          
+         /*
+         So basic approach is to count no of characters with even length 
+         d = no of characters with odd lenght
+         if(d>k) ans =0
+         */
+         
+         int ans=1;
+         int c=0,d=0;
+         for(int i=0;i<26;++i){
+             c+=z[i]/2;
+             z[i]%=2;
+             d+=z[i];
+         }
+         
+          
+              
+             int zz=c%k;
+             zz*=2;
+             
+             c/=k;
+             c*=2;
+            
+            // ;
+             //c*=2;
+            // cout<<d<<" "<<zz<<" "<<k<<"\n";
+             if((d+zz)>=k )d=1;
+             else d=0;
+             
+            // if(zz>=k)d=1;
+             
+             ans=max(ans,c+d);
+             cout<<ans<<"\n";
+          
+         
+    }
+}
