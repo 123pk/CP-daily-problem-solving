1d7ae3089dfba669771ca89047440fec78fc113d
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-01-12 23:10:35 +0530

    Update GRIDXOR.cpp

--- Day71/GRIDXOR.cpp
+++ Day71/GRIDXOR.cpp
@@ -3,3 +3,25 @@ Platform :- Codechef
 Contest :- Exun 2021-22 Division 2 (Rated)
 Approach :- we always have a grid of unity for , all ones in N*N matrix
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        int n;
+        cin>>n;
+        
+        int d=0;
+        while(d<n){
+        for(int i=0;i<n;++i){
+            cout<<1<<" ";
+        }
+        cout<<"\n";
+        d++;
+        }
+        
+    }
+}
