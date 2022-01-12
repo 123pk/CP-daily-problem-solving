ec70fb6f0382fde0334c9df948f0f5b667a3a52b
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-01-12 23:07:30 +0530

    Create SUMPARITY.cpp

---
+++ Day71/SUMPARITY.cpp
@@ -0,0 +1,8 @@
+/*
+Platform :- Codechef
+Contest :- Exun 2021-22 Division 2 (Rated)
+Approach :- We can identify 
+          1. "odd"  if 'n' is 'odd' and 'a' is 'odd' ,
+          2. "even" if 'n' is '1' and 'a' is even or 'n' is even and 'a' is 'odd'
+          3. else it is 'Impossible' to predict
+*/
