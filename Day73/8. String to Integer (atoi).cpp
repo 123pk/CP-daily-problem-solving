1e57f1a7e727b9e8095403d1e7653afd39094ab9
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-01-14 22:43:42 +0530

    Update 8. String to Integer (atoi).cpp

--- Day73/8. String to Integer (atoi).cpp
+++ Day73/8. String to Integer (atoi).cpp
@@ -2,3 +2,12 @@
 Platform :- Leetcode
 Approach :- Using stringstream builtin library of c++
 */
+class Solution {
+public:
+    int myAtoi(string s) {
+        stringstream sam(s);
+        int x=0;
+        sam>>x;
+        return x;
+    }
+};
