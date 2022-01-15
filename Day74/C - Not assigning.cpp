1e55d42fed593e078a66196e1dbb0e1b7bbbb5c2
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-01-15 22:10:53 +0530

    Create C - Not assigning.cpp

---
+++ Day74/C - Not assigning.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Round 766
+Approach :- We have only valid solutiono when we have a linear tree with maximum degree 2 and only two nodes with degree 1 , here we choose any of the end and start assining 
+             value '2' and '5' alternatively . 
+*/
