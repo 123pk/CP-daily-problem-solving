7d89ace474256ef1b3c74678b7a228403eff9582
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-01-15 22:08:29 +0530

    Create A - Not shading.cpp

---
+++ Day74/A - Not shading.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Round 766
+Approach :- if there is no black cell then we can't color our cell 'r',' c' , else we need atmost 2 steps , if any cell in row 'r' or any cell in coloumn 'c'
+           is colored black then we need atmax 1 , if the cell itself is black then 0
+*/
