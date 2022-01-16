67bdb3fa31dd63cca2c23933a876845c49c96907
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-01-16 22:56:22 +0530

    Update A - Equidistant Letters.cpp

--- Day75/A - Equidistant Letters.cpp
+++ Day75/A - Equidistant Letters.cpp
@@ -3,3 +3,17 @@ Platform :- Codeforces
 Contest :- Codeforces Educational contest 121
 Approach :- sort the string and all the characters appearing twice will come together hence distance between all of them would be same
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        string s;
+        cin>>s;
+        sort(s.begin(),s.end());
+        cout<<s<<"\n";
+    }
+}
