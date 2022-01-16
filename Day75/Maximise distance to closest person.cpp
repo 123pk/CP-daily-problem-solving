5f2ecda43c33b1babdbf6bafb313342380d22981
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-01-16 18:48:13 +0530

    Create Maximise distance to closest person.cpp

---
+++ Day75/Maximise distance to closest person.cpp
@@ -0,0 +1,8 @@
+/*
+Platform :- Leetcode
+Apporach :- Find all the indes which are not empty and now binarys search for each value in our sitting arrangement find the min distance between you and sitting person , max out of 
+           all such values is our answer.
+           
+          Approach 2:-  Another approach is that find the max distance between two adjacent sitting person , ans the max distance we can acheive is dist/2 , take care of cases when 
+                        you will sit at either end in that case max distance is distance to first sitting person.
+*/
