f60a71ae5abf32ddbf8afc2c511c028d984617fc
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-01-16 18:48:39 +0530

    Update Maximise distance to closest person.cpp

--- Day75/Maximise distance to closest person.cpp
+++ Day75/Maximise distance to closest person.cpp
@@ -6,3 +6,34 @@ Apporach :- Find all the indes which are not empty and now binarys search for ea
           Approach 2:-  Another approach is that find the max distance between two adjacent sitting person , ans the max distance we can acheive is dist/2 , take care of cases when 
                         you will sit at either end in that case max distance is distance to first sitting person.
 */
+class Solution {
+public:
+    int maxDistToClosest(vector<int>& seats) {
+        int sol=0;
+        
+        vector<int>used_seats;
+        for(int i=0;i<seats.size();++i){
+            if(seats[i]){
+                used_seats.push_back(i);
+            }
+        }
+        
+        int left,right;
+        for(int i=0;i<seats.size();++i){
+            auto it = lower_bound(used_seats.begin(),used_seats.end(),i);
+            if(it==used_seats.end())it--;
+            
+            if(it==used_seats.begin()){
+                sol=max(sol,abs(*it-i));
+            }
+            else{
+                if(*it==i)continue;
+                right = abs(*it-i);
+                it--;
+                left = abs(*it-i);
+                sol=max(sol,min(right,left));
+            }
+        }
+        return sol;
+    }
+};
