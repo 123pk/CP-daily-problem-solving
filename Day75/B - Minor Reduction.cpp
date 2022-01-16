07d8102727282b04a19c582781c12079758dec7b
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-01-16 23:19:00 +0530

    Create B - Minor Reduction.cpp

---
+++ Day75/B - Minor Reduction.cpp
@@ -0,0 +1,7 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Educational Round 121
+Approach :- If there is a pair of adjacent digits whose sum is >=10 then we will change them , starting from end we do this with the first pair we find 
+            else there is no such pair then it is optimal to choose the first two digits and replace with sum as this will give the maximum value than can
+            be achieved
+*/
