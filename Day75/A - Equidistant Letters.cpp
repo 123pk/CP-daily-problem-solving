a59f838e510dadc877799397bd3f051792400706
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-01-16 22:56:03 +0530

    Create A - Equidistant Letters.cpp

---
+++ Day75/A - Equidistant Letters.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Educational contest 121
+Approach :- sort the string and all the characters appearing twice will come together hence distance between all of them would be same
+*/
