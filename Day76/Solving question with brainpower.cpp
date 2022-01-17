2b963e7b89a52fbbd3aa87dbded92507db270cb1
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-01-17 21:14:22 +0530

    Update Solving question with brainpower.cpp

--- Day76/Solving question with brainpower.cpp
+++ Day76/Solving question with brainpower.cpp
@@ -4,3 +4,53 @@ Approach :- Take care of last index of current character , first make vector of 
          now for ith string if the last index is not equal to lasst index of current character then return false.
          else at the end return true.
 */
+   string temp1 = "";
+    string temp2 = "";
+    
+    int cnt = 1;
+    map<char , int> m1;
+    map<string , int> m2;
+    
+    for(int i=0 ; i<pattern.size() ; i++)
+    {
+        char ch = pattern[i];
+        
+        if(m1[ch]) temp1 += to_string(m1[ch]);
+        else
+        {
+            temp1 += to_string(cnt);
+            m1[ch] = cnt;
+            cnt++;
+        }
+    }
+    
+    
+    
+    int i=0;
+    int j=0;
+    cnt = 1;
+    
+    for(; i<=s.size() ; i++)
+    {
+        if(i == s.size() || s[i] == ' ' )
+        {
+            string temp = s.substr(j , i-j);
+            j = i+1;
+            
+            if(m2[temp]) temp2 += to_string(m2[temp]);
+            else
+            {
+                temp2 += to_string(cnt);
+                m2[temp] = cnt;
+                cnt++;
+            }
+            
+        }
+    }
+    
+    
+    if(temp1 == temp2) return true;
+    else return false;
+    
+}
+};
