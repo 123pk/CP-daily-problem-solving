5a655a70b8f2710352f572259bdd0fb03602508f
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-01-17 21:14:03 +0530

    Create Solving question with brainpower.cpp

---
+++ Day76/Solving question with brainpower.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Leetcode
+Approach :- Take care of last index of current character , first make vector of string of all words in second string and here take care of index of last strign,
+         now for ith string if the last index is not equal to lasst index of current character then return false.
+         else at the end return true.
+*/
