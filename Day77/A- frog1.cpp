6135af0bab8f8cb65796463ba0e3190e1c3b9169
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-01-18 20:59:07 +0530

    Update A- frog1.cpp

--- Day77/A- frog1.cpp
+++ Day77/A- frog1.cpp
@@ -1,6 +1,6 @@
 /*
 Platform :- Atcoder
-Approach :- Dp problem with 
+Approach :- Dp problem with ,for finding minimum cost to reach ith step we either jump from (i-1) step or (i-2) step 
            dp[0]=A[0]
            dp[1]=abs(A[1]-A[0]) , dp[2]=min(abs(A[2]-A[0],dp[1]+abs(A[2]-A[1])
            else dp[i]=min(dp[i-1]+abs(A[i]-A[i-1]) , dp[i-2]+abs(A[i]-A[i-2])
