3f0aa7f0d48cee3543d3b59ebe8d0d7b33814394
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-01-18 21:01:15 +0530

    Update B - Frog 2.cpp

--- Day77/B - Frog 2.cpp
+++ Day77/B - Frog 2.cpp
@@ -1,1 +1,7 @@
 
+/*
+Platform :- Atcoder
+Approach :- Dp problem with ,for finding minimum cost to reach ith step we either jump from (i-1) step or (i-2) step or ... (i-k) steps and min of all is choosen
+           dp[0]=A[0]
+            
+*/
