f8460862160831179dba205588e4a3bb90594a98
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-01-19 22:15:18 +0530

    Update Linked List Cycle II.cpp

--- Day78/Linked List Cycle II.cpp
+++ Day78/Linked List Cycle II.cpp
@@ -3,3 +3,30 @@ Platform :- Leetcode
 Approach :- Traverse through the linked list and maintain a set off all the adress of visited nodes of linked list if we got to one visited node then this is the 
             starting node of cycle and we return this else we return NULL at the end.
 */
+ 
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        set<ListNode*>used;
+        
+        if(!head)return NULL;
+        
+        
+        while(head){
+            if(used.size()){
+                if(used.find(head)==used.end()){
+                    used.insert(head);
+                    head=head->next;
+                }
+                else{
+                    return head;
+                }
+            }
+            else{
+                used.insert(head);
+                head=head->next;
+            } 
+        }
+        return NULL;
+    }
+};
