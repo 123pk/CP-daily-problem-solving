470892a05b330c89ad099b5dc849d10cb9dc0a50
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-01-21 19:23:43 +0530

    Create 1671. Minimum Number of Removals to Make Mountain Array.cpp

---
+++ Day80/1671. Minimum Number of Removals to Make Mountain Array.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Leetcode
+Approach :- Find LIS and LDS for each of the index , if LIS and LDS for each any index >=2 then the we can have a mountain sequence of size (LIS[i]+LDS[i])-1 , at the end we need 
+            to find number of items we need to remove and that are going to be ( n - size_of_longest_mountain_sequence)
+*/
