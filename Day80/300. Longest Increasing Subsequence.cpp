4d11f1dc387b20446fe0e55a1ff9127679e7920d
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-01-21 19:21:19 +0530

    Update 300. Longest Increasing Subsequence.cpp

--- Day80/300. Longest Increasing Subsequence.cpp
+++ Day80/300. Longest Increasing Subsequence.cpp
@@ -4,3 +4,22 @@ Approach :- We always have atleast a LIS of size '1' which will any item of arry
             and  if ( arr[i] >  arr[j] ) then we have a LIS till 'i' as LIS[i] = max (LIS[i-1] + 1, LIS[i] ).
             At the end we return the max value out of it .
 */
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int ans=1;
+        int n = nums.size();
+        vector<int>dp(n);
+        for(int i=0;i<nums.size();++i){
+            dp[i]=1;
+            for(int j=0;j<i;++j){
+                if(nums[i]>nums[j]){
+                    dp[i]=max(dp[i],dp[j]+1);
+                    ans=max(ans,dp[i]);
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
