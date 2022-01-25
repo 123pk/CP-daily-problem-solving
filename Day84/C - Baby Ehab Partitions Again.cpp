6f12885bf7a0d5479a65e1918845cc554079c57d
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-01-25 21:11:01 +0530

    Create 	C - Baby Ehab Partitions Again.cpp

---
+++ Day84/C - Baby Ehab Partitions Again.cpp
@@ -0,0 +1,7 @@
+/*
+Platform :- Codeforces
+Contest :- 
+Approach :- If the array is already good then we don't need to remove any element else the max no of elmenets we need to remove is 1, and that is the index of element with 
+           smallest LSB .
+           We use 0-1 Knapsack to find that the array is already divisble in to two subsequence of same sum or not if the sum is not odd initially.
+*/
