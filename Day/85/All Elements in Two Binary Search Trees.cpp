60344efa72109b562d229921869d6ab867b91587
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-01-26 19:36:19 +0530

    Update All Elements in Two Binary Search Trees.cpp

--- Day/85/All Elements in Two Binary Search Trees.cpp
+++ Day/85/All Elements in Two Binary Search Trees.cpp
@@ -2,3 +2,24 @@
 Platform :- Leetcode
 Approach :- Do dfs on binary and store in array for both roots then sort the array and return it.
 */
+class Solution {
+public:
+    
+    void dfs(TreeNode* node,vector<int>&ans){
+        if(!node)return;
+        ans.push_back(node->val);
+        dfs(node->left,ans);
+        dfs(node->right,ans);
+    }
+    
+    vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
+        vector<int>ans;
+        
+        dfs(root1,ans);
+        dfs(root2,ans);
+        
+        sort(ans.begin(),ans.end());
+        
+        return ans;
+    }
+};
