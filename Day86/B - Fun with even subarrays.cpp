bf461998d6a200e279f299c1622c5713e10d6354
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-01-28 14:03:45 +0530

    Create B - Fun with even subarrays.cpp

---
+++ Day86/B - Fun with even subarrays.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Codefoces
+Contest :- Codeforces Round 768 Div 2
+Approach :- Start with the last element if the second last is not equal then we make it equal to last and increment our size and count of operation and descent to i-c index
+            if we found some element which is already equal then we just add to our current pack and decrease the index .
+*/
