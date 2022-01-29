80bbf3b72e0e71a30a34f0ac26db72eb484e54f7
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-01-29 18:13:40 +0530

    Create Path with maximum probability .cpp

---
+++ Day87/Path with maximum probability .cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Leetcode
+Approach :- We will use same like dijkstara algorith and we will store the max weighted path ( contining probability) in this condition and use max heap in place of min heap 
+            to have max probability of path from start to all the other nodes
+*/
