da5e1ce9b3b05dd72ede8d277d72c7b16a070286
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-01-30 19:15:36 +0530

    Create E - Skiing.cpp

---
+++ Day88/E - Skiing.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Atcoder
+Contest :- Atcoder Beginner Contest 237
+Approach :- We will look for the maximum spanning tree or maximum value of happines if we visit all other nodes form node '1'.
+            We just print the max of all such values.
+*/
