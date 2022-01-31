81a0abce06aed0f365a507bcc3c67b17fac8b87c
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-01-31 22:27:49 +0530

    Create C -  Kill the monster .cpp

---
+++ Day89/C -  Kill the monster .cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Educational Round 122
+Approach :- Find greedily the most efficient way to use 'k' coins , start by using all 'k' coins in helath, then 'k-1'in health and '1' in attack and so on till '0' in health and 
+          'k' in attack and if in any state we can beat the monster we print "YES"
+*/
