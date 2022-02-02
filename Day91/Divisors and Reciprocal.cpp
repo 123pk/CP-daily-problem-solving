4ce1c4a5706608dd4c8b745e35489ef26a2fde18
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-02-02 23:00:18 +0530

    Create Divisors and Reciprocal.cpp

---
+++ Day91/Divisors and Reciprocal.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Codechef
+Contest :- Codechef Starters ( 2 feb 2022 ) 
+Approach :- We should know that [ f(n) = sum of reciprocal of divisors of any number 'n'  ] so "sum of divisors 'X' = n*f(n) 
+*/
