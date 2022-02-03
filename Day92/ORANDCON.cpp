72fe1c39b31fd36391ad12a1547111f26e44d28a
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-02-03 22:51:46 +0530

    Create ORANDCON.cpp

---
+++ Day92/ORANDCON.cpp
@@ -0,0 +1,8 @@
+/*
+Platform :- Codechef
+Contest :- Codechef Starters 3 feb
+Approach :- there are two cases if we have only one bit set then we have two case :-
+            1. When set bit is only 1 and values is '1' here we chhose 4,3,and 1 
+            2. if the set bit is only 1 and 'n' is not '1' here we choose 'n' ,0 and bit next to set bit in third one
+            we use greedy approach for seet bit count of 2 or more
+*/
