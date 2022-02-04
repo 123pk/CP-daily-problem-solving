44f2434c147dfabdd9ed978766211fff97e32b8f
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-02-04 22:47:35 +0530

    Update Contingous Array.cpp

--- Day93/Contingous Array.cpp
+++ Day93/Contingous Array.cpp
@@ -2,3 +2,20 @@
 Platform :- Leetcode
 Hint :- Hashmap & find 
 */
+class Solution{
+public:
+	int findMaxLength(vector<int> &nums){
+		unordered_map<int, int> mp;
+		mp[0] = -1;
+
+		int maxx = 0, sum = 0;
+		for (int i = 0; i < nums.size(); i++){
+			sum += nums[i] ? 1 : -1;
+			if (mp.find(sum) != mp.end())
+				maxx = max(i - mp[sum], maxx);
+			else
+				mp[sum] = i;
+		}
+		return maxx;
+	}
+};
