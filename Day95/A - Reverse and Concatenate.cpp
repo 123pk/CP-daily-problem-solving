db6a861e3bfe9870f3f0dc54a3c67ca1144d3a75
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-02-06 22:55:38 +0530

    Update A - Reverse and Concatenate.cpp

--- Day95/A - Reverse and Concatenate.cpp
+++ Day95/A - Reverse and Concatenate.cpp
@@ -1,1 +1,31 @@
 
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        int n,k;
+        cin>>n>>k;
+        
+        
+        string s;
+        cin>>s;
+        int f=0;
+        int ans = 1;
+        for(int i=0;i<(n/2);++i){
+            if(s[i]!=s[n-1-i]){
+                f=1;break;
+            }
+        }
+        
+        if(f && k){
+            ans++;
+        }
+        
+            cout<<ans<<"\n";
+        
+    }
+}
