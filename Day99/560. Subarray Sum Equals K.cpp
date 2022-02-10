8b87ffcc7c82331f8588c69e040abb9b67d46b6e
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-02-10 21:52:25 +0530

    Update 560. Subarray Sum Equals K.cpp

--- Day99/560. Subarray Sum Equals K.cpp
+++ Day99/560. Subarray Sum Equals K.cpp
@@ -2,3 +2,21 @@
 Platform :- Leetcode
 Approach :- Traverse through array and find that if (tot-k) is acheived earlier or not . 
 */
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int c=0,tot=0;
+        unordered_map<int,int>P;
+        P[0]=1;
+        for(int i=0;i<nums.size();++i)
+        {
+            tot+=nums[i];
+            if(P[tot-k])
+            {
+                c+=P[tot-k];
+            }
+            P[tot]++;
+        }
+        return c;
+    }
+};
