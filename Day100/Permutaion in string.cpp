596bc388d5803ca62becb25f67c6a60c68627fbf
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-02-11 19:58:19 +0530

    Create Permutaion in string.cpp

---
+++ Day100/Permutaion in string.cpp
@@ -0,0 +1,4 @@
+/*
+Platform :- Leetcode
+Approach :- For each index find the frequency for each character ( 'a' to 'z' ) , Now check in substring of  ( size_of_string1 ) from index [ 0 to n-size_of_string1 ] 
+*/
