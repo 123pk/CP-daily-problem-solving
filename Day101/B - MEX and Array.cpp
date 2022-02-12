39881292b124fdf1f5926fd267d1e128edcae0a5
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-02-12 22:39:21 +0530

    Create B - MEX and Array.cpp

---
+++ Day101/B - MEX and Array.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Global round 19
+Approach :- It is optimal to make set of all values of array size 1 in partition , that will give us (no_of_elements_in_segment + (is_zero_present_in_it) ) 
+*/
