138ed01b08cdd85d913ce3ce4f4d6d440fe09540
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-02-14 22:10:24 +0530

    Create B - Odd swap sort.cpp

---
+++ Day103/B - Odd swap sort.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Round 771 Div 2
+Approach :- You need to carefully understand the problem and you will that if there odd elements of array or even elements of array when taken seperately are not sorted then
+             we cannot sort else we can sort
+*/
