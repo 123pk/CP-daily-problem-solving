6af44ebb2cb1bdf6ae59d085edf002a8f7e5045e
Pawan Giri <49859885+123pk@users.noreply.github.com>
2022-02-14 22:08:35 +0530

    Create A - Reverse.cpp

---
+++ Day103/A - Reverse.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Round 771 Div 2
+Approach :- Find the first index where A[i] ! = i and find the values where 'i' is present in array and reverse the array from (i to index[i]) and print the array
+*/
